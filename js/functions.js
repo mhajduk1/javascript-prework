85e9a494ca72351bf8f5f16bc3c1979613c5a6e4
Michał Hajduk <michal.hajduk@e3.pl>
2019-09-12 09:10:53 +0200

    deleted-console.log

--- js/functions.js
+++ js/functions.js
@@ -2,7 +2,6 @@ function printMessage(msg){
 	var div = document.createElement('div');
 	div.innerHTML = msg;
 	document.getElementById('messages').appendChild(div);
-  console.log('wywołałem funkcję print message');
 }
 function clearMessages(){
 	document.getElementById('messages').innerHTML = '';
