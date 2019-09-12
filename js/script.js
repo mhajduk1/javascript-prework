e17a671a5a1243b9e42c9b085a084bb1b833f78f
Michał Hajduk <michal.hajduk@e3.pl>
2019-09-12 14:20:09 +0200

    add.maingamecode

--- js/script.js
+++ js/script.js
@@ -15,13 +15,6 @@ else if (playerInput == '3') {
 }
 printMessage('Twój ruch to: ' + playerMove);
 
-
-let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
-
-console.log('Gracz wpisał: ' + playerInput);
-
-let playerMove = 'nieznany ruch';
-
 if(playerInput == '1'){
   playerMove = 'kamień';
 }
@@ -31,5 +24,31 @@ else if (playerInput  == '2') {
 else if (playerInput == '3') {
 	playerMove = 'nożyce';
 }
-
-printMessage('Twój ruch to: ' + playerMove);
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+console.log('Wylosowana liczba to: ' + randomNumber);
+let computerMove = 'nieznany ruch';
+if(randomNumber == 1){
+  computerMove = 'kamień';
+  printMessage('Mój ruch to: ' + computerMove);
+}
+if(randomNumber == 2){
+  computerMove = 'papier';
+  printMessage('Mój ruch to: ' + computerMove);
+}
+if(randomNumber == 3){
+  computerMove = 'nożyce';
+  printMessage('Mój ruch to: ' + computerMove);
+}
+if( computerMove == 'kamień' && playerMove == 'papier'){
+  printMessage('Ty wygrywasz!');
+}if( computerMove == 'papier' && playerMove == 'nożyce'){
+  printMessage('Ty wygrywasz!');
+}if( computerMove == 'nożyce' && playerMove == 'kamień'){
+  printMessage('Ty wygrywasz!');
+}
+if (computerMove == playerMove) {
+	printMessage('Remis!');
+}
+if (playerInput == 'none') {
+	printMessage('Nie wykonałeś ruchu, automatycznie przegrywasz.');
+}
