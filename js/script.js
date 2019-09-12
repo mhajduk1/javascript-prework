7fd1f21cf7901d1c6d87b779d120de9714603d65
Michał Hajduk <michal.hajduk@e3.pl>
2019-09-12 14:03:01 +0200

    add.playermoves

--- js/script.js
+++ js/script.js
@@ -1,5 +1,16 @@
-let computerMove = 'kamien';
-let playerMove = 'papier';
-printMessage('Zagrałem ' +computerMove +'! Jeśli Twój ruch to papier, to wygrywasz! ' +playerMove);
- //clearMessages();
- 
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = 'nieznany ruch';
+
+if(playerInput == '1'){
+  playerMove = 'kamień';
+}
+else if (playerInput  == '2') {
+	playerMove = 'papier';
+}
+else if (playerInput == '3') {
+	playerMove = 'nożyce';
+}
+printMessage('Twój ruch to: ' + playerMove);
