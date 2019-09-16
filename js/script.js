926cb25d03dea5d4f556589011b368e65984bd6f
Michał Hajduk <michal.hajduk@e3.pl>
2019-09-16 15:46:07 +0200

    add css

--- js/script.js
+++ js/script.js
@@ -2,5 +2,4 @@ let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.
 let randomNumber = Math.floor(Math.random() * 3 + 1);
 let computerMove = getMoveName(randomNumber);
 let playerMove = getMoveName(playerInput);
-//warunki wygranych
 displayResult(computerMove ,playerMove);
