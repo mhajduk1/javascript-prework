05712adc1100e43f4b8b399ead958ac3a3830e2b
Michał Hajduk <michal.hajduk@e3.pl>
2019-09-18 12:14:49 +0200

    add buttons and styled them

--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,13 @@
+function playGame(playerInput){
+  //let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+  clearMessages();
+  let randomNumber = Math.floor(Math.random() * 3 + 1);
+  let computerMove = getMoveName(randomNumber);
+  let playerMove = getMoveName(playerInput);
+
+
+displayResult(computerMove ,playerMove);
+}
 function printMessage(msg){
 	var div = document.createElement('div');
 	div.innerHTML = msg;
--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
-let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
-let randomNumber = Math.floor(Math.random() * 3 + 1);
-let computerMove = getMoveName(randomNumber);
-let playerMove = getMoveName(playerInput);
-displayResult(computerMove ,playerMove);
+document.getElementById('play-rock').addEventListener('click', function(){
+playGame(1);
+});
+document.getElementById('play-paper').addEventListener('click', function(){
+playGame(2);
+});
+document.getElementById('play-scissors').addEventListener('click', function(){
+playGame(3);
+});
